8634a3edb3c7e81484a86d3450e431f6179a010a
Matthew McNaney <mcnaney@gmail.com>
2016-09-29 09:37:08 -0400

    Post and Patch cleaned up

--- class/Controller/ManagerController.php
+++ class/Controller/ManagerController.php
@@ -18,13 +18,16 @@ use properties\Factory\ManagerFactory as Factory;
 class ManagerController extends BaseController
 {
 
+    private $factory;
+
     public function __construct($module)
     {
         parent::__construct($module);
         // only admins may 
-        $this->adminPermissions = array('adminlist', 'create', 'edit', 'save', 'list');
+        $this->adminPermissions = array('adminlist', 'create', 'edit', 'save', 'list', 'post', 'delete', 'patch');
         $this->managerPermissions = array('edit', 'save');
         $this->userPermissions = array('userlist', 'view', 'checkUsername', 'checkEmail', 'checkCompanyName', 'Signin');
+        $this->factory = new Factory;
     }
 
     public function getHtmlView($data, \Request $request)
@@ -38,22 +41,21 @@ class ManagerController extends BaseController
             throw new \properties\Exception\PrivilegeMissing;
         }
 
-        $factory = new Factory;
-
         \Layout::addStyle('properties');
         switch ($command) {
             case 'list':
-                $content = $factory->listView();
+                $content = $this->factory->listView();
                 break;
 
             case 'create':
-                $content = $factory->create();
+                $content = $this->factory->create();
                 break;
 
             case 'signin':
-                $content = $factory->signInForm();
+                $content = $this->factory->signInForm();
 
             case 'view':
+                $content = 'html view';
                 break;
         }
         $view = new \phpws2\View\HtmlView($content);
@@ -63,20 +65,23 @@ class ManagerController extends BaseController
     public function getJsonView($data, \Request $request)
     {
         $command = $request->shiftCommand();
-
         if (empty($command)) {
             $command = 'list';
+        } elseif (is_numeric($command)) {
+            $managerId = (int)$command;
+            $command = 'view';
+        } else {
+            $managerId = $request->pullGetInteger('managerId', true);
         }
 
         if (!$this->allow($command, $request)) {
-            throw new \phpws2\Http\NotAcceptableException;
+            throw new \properties\Exception\PrivilegeMissing;
         }
-
-        $factory = new Factory;
 
         switch ($command) {
             case 'list':
-                $result = $factory->listingJson($factory->pullGetInteger('limit'), $factory->pullGetString('search'));
+                $result = $this->factory->listingJson($request->pullGetVarIfSet('limit',
+                                true), $request->pullGetString('search', true));
                 if (empty($result)) {
                     $json = array();
                 } else {
@@ -85,28 +90,63 @@ class ManagerController extends BaseController
                 break;
 
             case 'checkUsername':
-                $json = array('duplicate' => $factory->checkUsername($factory->pullGetString('username'),
-                            $factory->pullGetInteger('id')));
+                $json = array('duplicate' => $this->factory->checkUsername($request->pullGetString('username',
+                                    true), $request->pullGetInteger('id', true)));
                 break;
 
             case 'checkEmail':
-                $json = array('duplicate' => $factory->checkEmail($factory->pullGetString('email_address'),
-                            $factory->pullGetInteger('id')));
+                $json = array('duplicate' => $this->factory->checkEmail($request->pullGetString('email_address',
+                                    true), $request->pullGetInteger('id', true)));
                 break;
 
             case 'checkCompanyName':
-                $json = array('duplicate' => $factory->checkCompanyName($factory->pullGetString('company_name'),
-                            $factory->pullGetInteger('id')));
+                $json = array('duplicate' => $this->factory->checkCompanyName($request->pullGetString('company_name',
+                                    true), $request->pullGetInteger('id', true)));
+                break;
+            
+            case 'view':
+                $manager = $this->factory->load($managerId);
+                $json = $manager->getStringVars(null, 'password');
                 break;
         }
         $view = new \View\JsonView($json);
         return $view;
     }
 
+    public function delete(\Request $request)
+    {
+        if (!$this->allow('delete', $request)) {
+            throw new \properties\Exception\PrivilegeMissing;
+        }
+        $id = $request->pullGetInteger('managerId');
+        if ($id === false) {
+            throw new \phpws2\Exception\ValueNotSet('Manager id not set');
+        }
+        $json = array('success' => $this->factory->delete($id));
+        $view = new \View\JsonView($json);
+        $response = new \Response($view);
+        return $response;
+    }
+
     public function post(\Request $request)
     {
-        $factory = new Factory;
-        $json = $factory->post();
+        if (!$this->allow('post', $request)) {
+            throw new \properties\Exception\PrivilegeMissing;
+        }
+        $json = $this->factory->post($request);
+        $view = new \View\JsonView($json);
+        $response = new \Response($view);
+        return $response;
+    }
+
+    public function patch(\Request $request)
+    {
+        if (!$this->allow('patch', $request)) {
+            throw new \properties\Exception\PrivilegeMissing;
+        }
+        $param = $request->pullPatchString('param');
+        $json = array('success' => $this->factory->patch($request->pullPatchInteger('managerId'),
+                    $param, $request->pullPatchVar($param)));
         $view = new \View\JsonView($json);
         $response = new \Response($view);
         return $response;
