1dfef0ab3c358a0b9e92c2b8fc5aeaf5de9d7ebf
Matthew McNaney <mcnaney@gmail.com>
2016-09-29 09:37:47 -0400

    Removed variable pulls as they are in request now
    - also using the Base class's build method

--- class/Factory/Base.php
+++ class/Factory/Base.php
@@ -6,45 +6,20 @@ namespace properties\Factory;
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  */
-class Base extends \phpws2\ResourceFactory
+abstract class Base extends \phpws2\ResourceFactory
 {
 
-    public static function pullPostString($varname)
-    {
-        return trim(strip_tags(filter_input(INPUT_POST, $varname,
-                                FILTER_SANITIZE_STRING,
-                                FILTER_FLAG_NO_ENCODE_QUOTES)));
-    }
+    abstract protected function build();
 
-    public static function pullGetString($varname)
+    public function load($id)
     {
-        return trim(strip_tags(filter_input(INPUT_GET, $varname,
-                                FILTER_SANITIZE_STRING,
-                                FILTER_FLAG_NO_ENCODE_QUOTES)));
-    }
-
-    public static function pullPostCheck($varname)
-    {
-        return filter_input(INPUT_POST, $varname, FILTER_VALIDATE_BOOLEAN);
-    }
-
-    public static function pullPostInteger($varname)
-    {
-        return filter_input(INPUT_POST, $varname, FILTER_SANITIZE_NUMBER_INT);
-    }
-
-    public static function pullGetInteger($varname)
-    {
-        return filter_input(INPUT_GET, $varname, FILTER_SANITIZE_NUMBER_INT);
-    }
-
-    public static function build($id = 0, $resource)
-    {
-        if ($id) {
-            $resource->setId($id);
-            if (!parent::loadByID($resource)) {
-                throw new \Exception('Resource id not found:' . $id);
-            }
+        $resource = $this->build();
+        if (!$id) {
+            throw new \Exception('Missing id');
+        }
+        $resource->setId($id);
+        if (!parent::loadByID($resource)) {
+            throw new \Exception('Resource id not found:' . $id);
         }
         return $resource;
     }
