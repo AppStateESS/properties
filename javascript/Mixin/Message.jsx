31837e9fdd74de945719f70a5abbdd9911737f45
Matthew McNaney <mcnaney@gmail.com>
2016-10-06 13:01:17 -0400

    ES6 stuff

--- javascript/Mixin/Message.jsx
+++ javascript/Mixin/Message.jsx
@@ -1,38 +1,55 @@
-import React from 'react';
+import React from 'react'
 
 class Message extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    let icon = ''
+    switch (this.props.type) {
+      case 'danger':
+        icon = 'fa fa-exclamation-triangle'
+        break
+
+      case 'success':
+        icon = 'fa fa-thumbs-o-up'
+        break
+
+      case 'info':
+        icon = 'fa fa-info-circle'
+        break
+
+      case 'warning':
+        icon = 'fa fa-hand-paper-o'
+        break
     }
 
-    render() {
-        let icon = '';
-        switch (this.props.type) {
-            case 'danger':
-            icon = 'fa fa-exclamation-triangle';
-            break;
+    let messageType = 'lead alert alert-dismissible alert-' + this.props.type
+    return (
+      <div className={messageType} role="alert">
+        <button
+          type="button"
+          onClick={this.clearMessage}
+          className="close"
+          data-dismiss="alert"
+          aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <i className={icon}></i>&nbsp;
+        {this.props.message}
+      </div>
+    )
+  }
+}
 
-            case 'success':
-            icon = 'fa fa-thumbs-o-up';
-            break;
+Message.propTypes = {
+  type: React.PropTypes.string,
+  message: React.PropTypes.string
+}
 
-            case 'info':
-            icon = 'fa fa-info-circle';
-            break;
+Message.defaultProps = {
+  type: 'info'
+}
 
-            case 'warning':
-            icon = 'fa fa-hand-paper-o';
-            break;
-        }
-
-        let messageType = 'alert alert-dismissible alert-' + this.props.type;
-        return (
-            <div className={messageType} role="alert">
-                <button type="button" onClick={this.clearMessage} className="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <i className={icon}></i> {this.props.message}
-            </div>
-        );
-    }
-};
+export default Message
