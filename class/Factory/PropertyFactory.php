31e6966e8e540ee8ecdc58eca44bf0797dd8332f
Matthew McNaney <mcnaney@gmail.com>
2016-10-06 12:58:43 -0400

    Started listing code

--- class/Factory/PropertyFactory.php
+++ class/Factory/PropertyFactory.php
@@ -8,15 +8,22 @@
 
 namespace properties\Factory;
 
+use \properties\Resource\Property as Resource;
 use \phpws2\Database;
+use \properties\Exception\MissingInput;
 
 /**
  * Description of PropertyFactory
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  */
-class PropertyFactory
+class PropertyFactory extends BaseFactory
 {
+
+    protected function build()
+    {
+        return new Resource;
+    }
 
     public function delete($id)
     {
@@ -29,7 +36,7 @@ class PropertyFactory
         $db->delete();
         $this->removePhotos($id);
     }
-    
+
     public function removePhotos($id)
     {
         if (empty($id) || !is_numeric($id)) {
@@ -45,8 +52,8 @@ class PropertyFactory
             $photoFactory->delete($photo);
         }
     }
-    
-    public function getPhotos($id, $as_resource=false)
+
+    public function getPhotos($id, $as_resource = false)
     {
         if (empty($id) || !is_numeric($id)) {
             throw new \Exception('Property id invalid');
@@ -56,5 +63,30 @@ class PropertyFactory
         $img_tbl->addFieldConditional('pid', $id);
         $result = $db->selectAsResources('\properties\Resource\Photo');
         return $result;
-    }   
+    }
+
+    public function listing($manager_id = 0, $search = null, $limit = 100)
+    {
+        $db = Database::getDB();
+        $tbl = $db->addTable('properties');
+        if ((int) $limit <= 0) {
+            $limit = 100;
+        }
+        $db->setLimit($limit);
+        if (!empty($search)) {
+            $search_string = "%$search%";
+            $s1 = $db->createConditional($tbl->getField('name'), $search_string,
+                    'like');
+            $s2 = $db->createConditional($tbl->getField('address'),
+                    $search_string, 'like');
+            $conditional = $db->createConditional($s1, $s2, 'or');
+            $db->addConditional($conditional);
+        }
+        if ($manager_id) {
+            $tbl->addFieldConditional('contact_id', $manager_id);
+        }
+        $result = $db->select();
+        return $result;
+    }
+
 }
