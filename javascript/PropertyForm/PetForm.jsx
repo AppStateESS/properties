d3f40df7a59ad24333025c68b1dd276376701ca1
Matthew McNaney <mcnaney@gmail.com>
2016-10-11 12:19:30 -0400

    expanded functionality

--- javascript/PropertyForm/PetForm.jsx
+++ javascript/PropertyForm/PetForm.jsx
@@ -9,37 +9,50 @@ class PetForm extends React.Component {
 
   render() {
     return (
-      <div className="row">
-        <div className="col-sm-6">
-          <label>Deposit</label>
-          (Refundable)
-          <div className="input-group">
-            <span className="input-group-addon">$</span>
-            <InputField
-              name="pet_deposit"
-              type="text"
-              value={this.props.property.pet_deposit}
-              disabled={!this.props.show}
-              change={this.props.setValue.bind(this, 'pet_deposit')}/>
-            <span className="input-group-addon">.00</span>
+      <div>
+        <div className="row marginBottom">
+          <div className="col-sm-6">
+            <label>Deposit</label>
+            (Refundable)
+            <div className="input-group">
+              <span className="input-group-addon">$</span>
+              <InputField
+                name="pet_deposit"
+                type="text"
+                value={this.props.property.pet_deposit}
+                disabled={!this.props.show}
+                change={this.props.setValue.bind(this, 'pet_deposit')}/>
+              <span className="input-group-addon">.00</span>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <label>Fee</label>
+            (Non-refundable)
+            <div className="input-group">
+              <span className="input-group-addon">$</span>
+              <InputField
+                name="pet_fee"
+                type="text"
+                disabled={!this.props.show}
+                value={this.props.property.pet_fee}
+                change={this.props.setValue.bind(this, 'pet_fee')}/>
+              <span className="input-group-addon">.00</span>
+            </div>
           </div>
         </div>
-        <div className="col-sm-6">
-          <label>Fee</label>
-          (Non-refundable)
-          <div className="input-group">
-            <span className="input-group-addon">$</span>
-            <InputField
-              name="pet_fee"
-              type="text"
-              value={this.props.property.pet_fee}
-              change={this.props.setValue.bind(this, 'pet_fee')}/>
-            <span className="input-group-addon">.00</span>
+        <div className="row">
+          <div className="col-sm-12">
+            <label htmlFor="pet-type">Allowed pet types (i.e. cats, dogs)</label>
+            <textarea
+              disabled={!this.props.show}
+              className="form-control"
+              id="pet-type"
+              value={this.props.property.pet_type}
+              onChange={this.props.setValue.bind(this, 'pet_type')}></textarea>
           </div>
         </div>
       </div>
     )
-
   }
 }
 
