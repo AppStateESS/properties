d507313b275e32ff5fec0a48738496394fa6b2c3
Matthew McNaney <mcnaney@gmail.com>
2016-10-12 09:01:04 -0400

    shifted buttons around
    still unsure of layout. work in progress

--- javascript/PropertyForm/PropertyFeatures.jsx
+++ javascript/PropertyForm/PropertyFeatures.jsx
@@ -12,99 +12,83 @@ class PropertyFeatures extends React.Component {
     const property = this.props.property
     return (
       <div>
-        <h3>Features</h3>
+        <h3>Conditions</h3>
+        <p>
+          <BooleanButton
+            current={property.efficiency}
+            label={['Efficiency', 'Not an efficiency']}
+            icon={true}
+            handleClick={this.props.setValue.bind(this, 'efficiency')}/>
+        </p>
+        <p>
+          <BooleanButton
+            current={property.sublease}
+            label={['Tenant may sublease', 'Tenant may not sublease']}
+            icon={true}
+            handleClick={this.props.setValue.bind(this, 'sublease')}/>
+        </p>
+        <p>
+          <BooleanButton
+            current={property.window_number}
+            icon={true}
+            label={['Windows in unit', 'Windowless unit']}
+            handleClick={this.props.setValue.bind(this, 'window_number')}/>
+        </p>
         <div className="row">
           <div className="col-sm-6">
-            <table className="table">
-              <tbody>
-                <tr>
-                  <td>Utilities included</td>
-                  <td className="text-right">
-                    <BooleanButton
-                      current={property.utilities_inc}
-                      handleClick={this.props.setValue.bind(this, 'utilities_inc')}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Efficiency</td>
-                  <td className="text-right">
-                    <BooleanButton
-                      current={property.efficiency}
-                      handleClick={this.props.setValue.bind(this, 'efficiency')}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Subleasing permitted</td>
-                  <td className="text-right">
-                    <BooleanButton
-                      current={property.sublease}
-                      handleClick={this.props.setValue.bind(this, 'sublease')}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Furnished</td>
-                  <td className="text-right">
-                    <BooleanButton
-                      current={property.furnished}
-                      handleClick={this.props.setValue.bind(this, 'furnished')}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Dishwasher</td>
-                  <td className="text-right">
-                    <BooleanButton
-                      current={property.dishwasher}
-                      handleClick={this.props.setValue.bind(this, 'dishwasher')}/>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <h3>Amenities</h3>
+            <p>
+              <BooleanButton
+                current={property.utilities_inc}
+                label={['Utilities included', 'Utilities not included']}
+                icon={true}
+                handleClick={this.props.setValue.bind(this, 'utilities_inc')}/>
+            </p>
+            <p>
+              <BooleanButton
+                current={property.furnished}
+                label={['Furnished', 'Not furnished']}
+                icon={true}
+                handleClick={this.props.setValue.bind(this, 'furnished')}/>
+            </p>
+            <p>
+              <BooleanButton
+                current={property.dishwasher}
+                label={['Dishwasher included', 'No dishwasher']}
+                icon={true}
+                handleClick={this.props.setValue.bind(this, 'dishwasher')}/>
+            </p>
+            <p>
+              <BooleanButton
+                current={property.airconditioning}
+                icon={true}
+                label={['Air conditioning', 'No air conditioning']}
+                handleClick={this.props.setValue.bind(this, 'airconditioning')}/>
+            </p>
+            <p>
+              <BooleanButton
+                current={property.appalcart}
+                icon={true}
+                label={['On Appalcart route', 'Not on Appalcart route']}
+                handleClick={this.props.setValue.bind(this, 'appalcart')}/>
+            </p>
+            <p>
+              <BooleanButton
+                current={property.clubhouse}
+                icon={true}
+                label={['Club house available', 'No club house']}
+                handleClick={this.props.setValue.bind(this, 'clubhouse')}/>
+            </p>
+            <p>
+              <BooleanButton
+                current={property.workout_room}
+                icon={true}
+                label={['Workout room', 'No workout room']}
+                handleClick={this.props.setValue.bind(this, 'workout_room')}/>
+            </p>
           </div>
           <div className="col-sm-6">
-            <table className="table">
-              <tbody>
-                <tr>
-                  <td>Windows (i.e. not in basement)</td>
-                  <td className="text-right">
-                    <BooleanButton
-                      current={property.window_number}
-                      handleClick={this.props.setValue.bind(this, 'window_number')}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Air Conditioning</td>
-                  <td className="text-right">
-                    <BooleanButton
-                      current={property.airconditioning}
-                      handleClick={this.props.setValue.bind(this, 'airconditioning')}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Appalcart route</td>
-                  <td className="text-right">
-                    <BooleanButton
-                      current={property.appalcart}
-                      handleClick={this.props.setValue.bind(this, 'appalcart')}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Clubhouse</td>
-                  <td className="text-right">
-                    <BooleanButton
-                      current={property.clubhouse}
-                      handleClick={this.props.setValue.bind(this, 'clubhouse')}/>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Workout room</td>
-                  <td className="text-right">
-                    <BooleanButton
-                      current={property.workout_room}
-                      handleClick={this.props.setValue.bind(this, 'workout_room')}/>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <label>something here?</label>
           </div>
         </div>
       </div>
