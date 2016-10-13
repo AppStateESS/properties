6e5fe142b6d379be91082cea7e8c17ed754ec6ba
Matthew McNaney <mcnaney@gmail.com>
2016-10-13 13:21:08 -0400

    Form subsections in own files.

---
+++ javascript/PropertyForm/Fees.jsx
@@ -0,0 +1,99 @@
+'use strict'
+import React from 'react'
+import InputField from '../Mixin/InputField.jsx'
+import BooleanButton from '../Mixin/BooleanButton.jsx'
+
+export default class Fees extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    const property = this.props.property
+    return (
+      <div>
+        <h3>Deposits and Fees</h3>
+        <div className="row">
+          <div className="col-sm-6">
+            <label>Security deposit</label>
+            <BooleanButton
+              current={property.security_refund}
+              label={['Deposit refunded', 'Nonrefundable']}/>
+            <div className="input-group">
+              <span className="input-group-addon">$</span>
+              <InputField
+                name="security_amt"
+                type="text"
+                maxLength={6}
+                value={property.security_amt}
+                change={this.props.setValue.bind(this, 'security_amt')}/>
+              <span className="input-group-addon">.00</span>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <label>Administrative Fee</label>
+            <BooleanButton
+              current={property.admin_fee_refund}
+              label={['Admin fee refunded', 'Nonrefundable']}/>
+            <div className="input-group">
+              <span className="input-group-addon">$</span>
+              <InputField
+                name="admin_fee_amt"
+                maxLength={6}
+                type="text"
+                value={property.admin_fee_amt}
+                change={this.props.setValue.bind(this, 'admin_fee_amt')}/>
+              <span className="input-group-addon">.00</span>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-6">
+            <label>Cleaning fee</label>
+            <BooleanButton
+              current={property.clean_fee_refund}
+              label={['Deposit refunded', 'Nonrefundable']}/>
+            <div className="input-group">
+              <span className="input-group-addon">$</span>
+              <InputField
+                name="clean_fee_amt"
+                type="text"
+                maxLength={6}
+                value={property.clean_fee_amt}
+                change={this.props.setValue.bind(this, 'clean_fee_amt')}/>
+              <span className="input-group-addon">.00</span>
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <label>Parking Fee</label>
+            <div className="input-group">
+              <span className="input-group-addon">$</span>
+              <InputField
+                name="parking_fee"
+                maxLength={6}
+                type="text"
+                value={property.parking_fee}
+                change={this.props.setValue.bind(this, 'parking_fee')}/>
+              <span className="input-group-addon">.00</span>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <label>Other fees</label>
+            <textarea
+              className="form-control"
+              value={property.other_fees}
+              name="other_fees"></textarea>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Fees.propTypes = {
+  property: React.PropTypes.object,
+  setValue: React.PropTypes.func
+}
---
+++ javascript/PropertyForm/Rooms.jsx
@@ -0,0 +1,92 @@
+'use strict'
+import React from 'react'
+import ButtonGroup from '../Mixin/ButtonGroup.jsx'
+import Range from '../Mixin/Range.js'
+import classnames from 'classnames'
+import bindMethods from '../Mixin/Bind.js'
+
+export default class Rooms extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      half: false
+    }
+    bindMethods([
+      'updateBathroom', 'updateBedroom'
+    ], this)
+
+  }
+
+  updateBedroom(bedrooms) {
+    if (typeof bedrooms === 'object') {
+      bedrooms = Number(bedrooms.target.value)
+    } else {
+      bedrooms = Number(bedrooms)
+    }
+    if (bedrooms >= 1 && bedrooms <= 6) {
+      this.props.setValue('bedroom_no', bedrooms)
+    }
+  }
+
+  updateBathroom(bathrooms) {
+    if (typeof bathrooms === 'object') {
+      bathrooms = Number(bathrooms.target.value)
+    } else {
+      bathrooms = Number(bathrooms)
+    }
+    if (bathrooms >= 1 && bathrooms <= 7) {
+      this.props.setValue('bathroom_no', bathrooms)
+    }
+  }
+
+  render() {
+    const {property} = this.props
+    let bathrooms = Range(property.bathroom_no, this.state.half)
+    let bedrooms = Range(property.bedroom_no)
+    const halfcn = classnames('marginLeft', 'btn', this.state.half
+      ? 'btn-success'
+      : 'btn-default')
+    return (
+      <div className="row bg-info">
+        <div className="col-sm-6">
+          <label>Bedrooms</label>
+          <input
+            type="text"
+            size="1"
+            onClick={this.select}
+            onChange={this.updateBedroom}
+            value={property.bedroom_no}
+            className="single-input"/><br/>
+          <ButtonGroup
+            buttons={bedrooms}
+            match={property.bedroom_no}
+            handle={this.updateBedroom}
+            activeColor="success"/>
+        </div>
+        <div className="col-sm-6">
+          <div>
+            <label>Bathrooms</label>
+            <input
+              type="text"
+              size="3"
+              onChange={this.updateBathroom}
+              onClick={this.select}
+              value={property.bathroom_no}
+              className="single-input"/>
+          </div>
+          <ButtonGroup
+            buttons={bathrooms}
+            match={property.bathroom_no}
+            handle={this.updateBathroom}
+            activeColor="success"/>
+          <button className={halfcn} onClick={this.half}>1/2</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+Rooms.propTypes = {
+  property: React.PropTypes.object,
+  setValue: React.PropTypes.func
+}
