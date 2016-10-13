920281f61c326001baf218d76a4056af1f7b7dd4
Matthew McNaney <mcnaney@gmail.com>
2016-10-13 13:24:46 -0400

    Many changes

--- javascript/PropertyForm/PropertyForm.jsx
+++ javascript/PropertyForm/PropertyForm.jsx
@@ -1,17 +1,27 @@
 'use strict'
 import React from 'react'
-import PetForm from './PetForm.jsx'
+import moment from 'moment'
+
+import bindMethods from '../Mixin/Bind.js'
+import {DateField} from 'react-date-picker'
 import DecodeUrl from '../Mixin/DecodeUrl.js'
-import InputField from '../Mixin/InputField.jsx'
 import PropertyObject from '../Mixin/PropertyObject.js'
-import PropertyFeatures from './PropertyFeatures.jsx'
+import InputField, {RequiredIcon} from '../Mixin/InputField.jsx'
 import ErrorPage from '../Mixin/ErrorPage.jsx'
 import ButtonGroup from '../Mixin/ButtonGroup.jsx'
 import BooleanButton from '../Mixin/BooleanButton.jsx'
+import Range from '../Mixin/Range.js'
+import Dollarize from '../Mixin/Dollarize.jsx'
+
+import Pets from './Pets.jsx'
+import Fees from './Fees.jsx'
+import Rooms from './Rooms.jsx'
+import Features from './Features.jsx'
+import Utilities from './Utilities.jsx'
+
 import 'react-date-picker/index.css'
-import {DateField} from 'react-date-picker'
-import moment from 'moment'
-import classnames from 'classnames'
+
+/* global $ */
 
 export default class PropertyForm extends React.Component {
   constructor() {
@@ -20,21 +30,57 @@ export default class PropertyForm extends React.Component {
     this.managerId = url.values['managerId']
     this.state = {
       property: PropertyObject,
+      manager: {},
       petForm: PropertyObject.pets_allowed,
-      half: false
+      errors : { name : false, address: false, monthly_rent: false}
     }
-    this.half = this.half.bind(this)
-    this.setValue = this.setValue.bind(this)
-    this.setMoveIn = this.setMoveIn.bind(this)
-    this.togglePets = this.togglePets.bind(this)
-    this.updateHeatType = this.updateHeatType.bind(this)
-    this.updateLease = this.updateLease.bind(this)
-    this.updateBedroom = this.updateBedroom.bind(this)
-    this.updateBathroom = this.updateBathroom.bind(this)
-    this.updateStudentType = this.updateStudentType.bind(this)
+    const methods = [
+      'half',
+      'setValue',
+      'setMoveIn',
+      'togglePets',
+      'updateHeatType',
+      'checkForm'
+    ]
+    bindMethods(methods, this)
+  }
+
+  checkForm(e) {
+    e.preventDefault()
+
+    const property = this.state.property
+    let errors = this.state.errors
+    let errorFound = false
+    if (property.name.length === 0) {
+      errors.name = true
+      errorFound = true
+    } else {
+      errors.name = false
+    }
+
+    if (property.address.length === 0){
+      errors.address = true
+      errorFound = true
+    } else {
+      errors.address = false
+    }
+
+    if (errorFound) {
+      this.setState({errors : errors})
+      this.refs.PropertyForm.scrollIntoView()
+    }
+  }
+
+  componentDidMount() {
+    $.getJSON('./properties/Manager/' + this.managerId, {}).done(function (data) {
+      this.setState({manager: data})
+    }.bind(this))
   }
 
   setValue(varname, value) {
+    if (typeof value === 'object' && value.target !== undefined) {
+      value = value.target.value
+    }
     let property = this.state.property
     property[varname] = value
     this.setState({property})
@@ -68,30 +114,60 @@ export default class PropertyForm extends React.Component {
       {
         value: 1,
         label: 'Heat pump'
-      },
-      {
+      }, {
         value: 2,
         label: 'Oil'
-      },
-      {
+      }, {
         value: 3,
         label: 'Propane'
-      },
-      {
+      }, {
         value: 4,
         label: 'Electric baseboard'
-      },
-      {
+      }, {
         value: 5,
         label: 'Kerosene'
-      },
-      {
+      }, {
         value: 6,
         label: 'Woodstove/Fireplace'
-      },
-      {
+      }, {
         value: 7,
         label: 'Natural gas'
+      }
+    ]
+  }
+
+  laundryTypes() {
+    return [
+      {
+        value: 0,
+        label: 'No laundry'
+      }, {
+        value: 1,
+        label: 'Washer/Dryer in unit'
+      }, {
+        value: 2,
+        label: 'Laundry room on premises'
+      }, {
+        value: 3,
+        label: 'Washer/Dryer hook ups in unit'
+      }
+    ]
+  }
+
+  trashTypes() {
+    return [
+      {
+        value: 0,
+        label: 'No pickup or bins'
+      }, {
+        value: 1,
+        label: 'Curbside pickup'
+      }, {
+        value: 2,
+        label: 'Trash only, no recycling bins'
+      }, {
+        value: 3,
+        label: 'Both bins on site'
       }
     ]
   }
@@ -112,34 +188,42 @@ export default class PropertyForm extends React.Component {
     }
   }
 
-  updateLease(a) {
-    this.setValue('lease_type', a)
-  }
-
-  updateBedroom(bedrooms) {
-    if (typeof bedrooms === 'object') {
-      bedrooms = Number(bedrooms.target.value)
+  updateParking(parking) {
+    if (typeof parking === 'object') {
+      parking = Number(parking.target.value)
     } else {
-      bedrooms = Number(bedrooms)
+      parking = Number(parking)
     }
-    if (bedrooms >= 1 && bedrooms <= 6) {
-      this.setValue('bedroom_no', bedrooms)
-    }
-  }
-
-  updateBathroom(bathrooms) {
-    if (typeof bathrooms === 'object') {
-      bathrooms = Number(bathrooms.target.value)
-    } else {
-      bathrooms = Number(bathrooms)
-    }
-    if (bathrooms >= 1 && bathrooms <= 7) {
-      this.setValue('bathroom_no', bathrooms)
+    if (parking >= 1 && parking <= 6) {
+      this.setValue('parking_per_unit', parking)
     }
   }
 
-  updateStudentType(type) {
-    this.setValue('student_type', type)
+  internetTypes() {
+    return [
+      {
+        value: 1,
+        label: 'Dial up'
+      }, {
+        value: 2,
+        label: 'DSL'
+      }, {
+        value: 3,
+        label: 'Wireless'
+      }, {
+        value: 4,
+        label: 'Satellite'
+      }, {
+        value: 5,
+        label: 'Cable'
+      }, {
+        value: 6,
+        label: 'DSL/Cable'
+      }, {
+        value: 7,
+        label: 'Fiber'
+      }
+    ]
   }
 
   updateHeatType(type) {
@@ -152,17 +236,6 @@ export default class PropertyForm extends React.Component {
       heatType.splice(index, 1)
     }
     this.setValue('heat_type', heatType)
-  }
-
-  getRangeButtons(current, deci = false) {
-    let range = []
-    const start = deci
-      ? 1.5
-      : 1
-    for (let i = start; i < 7; i++) {
-      range.push({value: i, label: i})
-    }
-    return range
   }
 
   studentType() {
@@ -191,25 +264,27 @@ export default class PropertyForm extends React.Component {
     })
   }
 
+  dollarize(input) {
+    return <Dollarize>{input}</Dollarize>
+  }
+
   render() {
     if (this.managerId === undefined || this.managerId === 0) {
       return <ErrorPage message="I can't go for that"/>
     }
     const property = this.state.property
-    let bedrooms = this.getRangeButtons(property.bedroom_no)
-    let bathrooms = this.getRangeButtons(property.bathroom_no, this.state.half)
+    let parking = Range(property.parking_per_unit)
 
-    const halfcn = classnames('marginLeft', 'btn', this.state.half
-      ? 'btn-success'
-      : 'btn-default')
     return (
-      <div className="property-form">
+      <form ref="PropertyForm" className="property-form" method="post" action="./properties/Property">
+        <h2>Property for {this.state.manager.company_name}</h2>
         <div className="row bg-info">
           <div className="col-sm-12 ">
             <InputField
               name="name"
               label="Title"
               value={property.name}
+              errorMessage={this.state.errors.name ? 'Title may not be empty': null}
               change={this.setValue.bind(this, 'name')}
               required={true}/>
           </div>
@@ -220,27 +295,38 @@ export default class PropertyForm extends React.Component {
               name="address"
               label="Address"
               type="text"
+              errorMessage={this.state.errors.address ? 'Address may not be empty': null}
               value={property.address}
               change={this.setValue.bind(this, 'address')}
               required={true}/>
           </div>
         </div>
-
         <div className="row bg-info">
-          <div className="col-sm-12">
-            <div className="form-inline">
+          <div className="col-sm-5">
+            <label>Monthly rent<RequiredIcon/></label>
               <InputField
                 name="monthly_rent"
-                label="Monthly rent"
-                type="text"
+                type="type"
+                wrap={this.dollarize}
                 value={property.monthly_rent}
                 change={this.setValue.bind(this, 'monthly_rent')}
                 required={true}/>
+          </div>
+          <div className="col-sm-7">
+            <div style={{marginBottom:'.5em'}}>
               <ButtonGroup
                 buttons={this.getLeaseType()}
                 match={property.lease_type}
-                handle={this.updateLease}
-                activeColor="success"/></div>
+                handle={this.setValue.bind(this, 'lease_type')}
+                activeColor="success"/>
+            </div>
+            <div>
+              <BooleanButton
+                current={property.efficiency}
+                label={['Efficiency', 'Not an efficiency']}
+                icon={true}
+                handleClick={this.setValue.bind(this, 'efficiency')}/>
+            </div>
           </div>
         </div>
         <div className="row">
@@ -260,7 +346,15 @@ export default class PropertyForm extends React.Component {
               <option value="5">per Semester</option>
               <option value="6">School year (two semesters)</option>
             </select>
-
+            <div style={{
+              marginTop: '.5em'
+            }}>
+              <BooleanButton
+                current={property.sublease}
+                label={['Tenant may sublease', 'Tenant may not sublease']}
+                icon={true}
+                handleClick={this.setValue.bind(this, 'sublease')}/>
+            </div>
           </div>
           <div className="col-sm-6">
             <label>Move-in date</label>
@@ -270,54 +364,31 @@ export default class PropertyForm extends React.Component {
               value={this.getMoveInDate()}/>
           </div>
         </div>
-        <div className="row bg-info">
+        <Rooms property={property} setValue={this.setValue}/>
+        <div className="row">
           <div className="col-sm-6">
-            <label>Bedrooms</label>
-            <input
-              type="text"
-              size="1"
-              onClick={this.select}
-              onChange={this.updateBedroom}
-              value={property.bedroom_no}
-              className="single-input"/><br/>
-            <ButtonGroup
-              buttons={bedrooms}
-              match={property.bedroom_no}
-              handle={this.updateBedroom}
-              activeColor="success"/>
-          </div>
-          <div className="col-sm-6">
-            <div>
-              <label>Bathrooms</label>
+            <div className="pull-left">
+              <label>Parking spaces per unit</label>
               <input
                 type="text"
-                size="3"
-                onChange={this.updateBathroom}
+                size="2"
+                onChange={this.updateParking}
                 onClick={this.select}
-                value={property.bathroom_no}
+                value={property.parking_per_unit}
                 className="single-input"/>
             </div>
             <ButtonGroup
-              buttons={bathrooms}
-              match={property.bathroom_no}
-              handle={this.updateBathroom}
-              activeColor="success"/>
-            <button className={halfcn} onClick={this.half}>1/2</button>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-12">
-            <label>Student preference</label>
-            <ButtonGroup
-              buttons={this.studentType()}
-              match={property.student_type}
-              handle={this.updateStudentType}
+              buttons={parking}
+              match={property.parking_per_unit}
+              handle={this.updateParking}
               activeColor="success"/>
           </div>
         </div>
+        <SubmitForm check={this.checkForm}/>
         <div className="row bg-info">
           <div className="col-sm-12">
-            <label>Heating</label> <small>(Click all that apply)</small>
+            <label>Heating</label>
+            <small>(Click all that apply)</small>
             <ButtonGroup
               buttons={this.heatingTypes()}
               match={property.heat_type}
@@ -325,29 +396,84 @@ export default class PropertyForm extends React.Component {
               activeColor="success"/>
           </div>
         </div>
-
-        <PropertyFeatures property={property} setValue={this.setValue}/>
-
-        <h3>Pets</h3>
-        <BooleanButton
-          label={['Pets allowed', 'Pets not allowed']}
-          icon={['fa fa-check', 'fa fa-times']}
-          current={property.pets_allowed}
-          handleClick={this.togglePets.bind(this, !property.pets_allowed)}/>
-        <div className="marginTop">
-          <div style={this.obscurePetForm()}>
-            <PetForm
-              property={this.state.property}
-              setValue={this.setValue}
-              show={this.state.petForm}/>
+        <div className="row">
+          <div className="col-sm-12">
+            <label>Internet</label>
+            <ButtonGroup
+              buttons={this.internetTypes()}
+              match={property.internet_type}
+              handle={this.setValue.bind(this, 'internet_type')}
+              activeColor="success"/>
           </div>
         </div>
-
-      </div>
+        <div className="row bg-info">
+          <div className="col-sm-12">
+            <label>Laundry</label>
+            <ButtonGroup
+              buttons={this.laundryTypes()}
+              match={property.laundry_type}
+              handle={this.setValue.bind(this, 'laundry_type')}
+              activeColor="success"/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <label>Trash and Recycling</label>
+            <ButtonGroup
+              buttons={this.trashTypes()}
+              match={property.trash_type}
+              handle={this.setValue.bind(this, 'trash_type')}
+              activeColor="success"/>
+          </div>
+        </div>
+        <div className="row bg-info">
+          <div className="col-sm-12">
+            <label>Student preference</label>
+            <ButtonGroup
+              buttons={this.studentType()}
+              match={property.student_type}
+              handle={this.setValue.bind(this, 'student_type')}
+              activeColor="success"/>
+          </div>
+        </div>
+        <Features property={property} setValue={this.setValue}/>
+        <SubmitForm check={this.checkForm}/>
+        <div className="row">
+          <div className="col-sm-12">
+            <h3>Pets</h3>
+            <BooleanButton
+              label={['Pets allowed', 'Pets not allowed']}
+              icon={['fa fa-check', 'fa fa-times']}
+              current={property.pets_allowed}
+              handleClick={this.togglePets.bind(this, !property.pets_allowed)}/>
+            <div style={this.obscurePetForm()}>
+              <Pets
+                property={this.state.property}
+                setValue={this.setValue}
+                show={this.state.petForm}/>
+            </div>
+          </div>
+        </div>
+        <Fees property={property} setValue={this.setValue}/>
+        <Utilities property={property} setValue={this.setValue}/>
+        <SubmitForm check={this.checkForm}/>
+      </form>
     )
   }
 }
-
 PropertyForm.propTypes = {
   address: React.PropTypes.string
+}
+
+const SubmitForm = ({check}) => {
+  return (
+    <div className="submit-form">
+      <button type="submit" className="btn btn-primary btn-lg" onClick={check}>
+        <i className="fa fa-save"></i>&nbsp;Save property</button>
+    </div>
+  )
+}
+
+SubmitForm.propTypes = {
+  check: React.PropTypes.func
 }
