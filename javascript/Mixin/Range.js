066f23dd45b1d8174fd97854ab52521145109937
Matthew McNaney <mcnaney@gmail.com>
2016-10-13 13:20:49 -0400

    Creates a range from 1 to 6 with halves

---
+++ javascript/Mixin/Range.js
@@ -0,0 +1,10 @@
+export default function Range(current, deci = false) {
+  let range = []
+  const start = deci
+    ? 1.5
+    : 1
+  for (let i = start; i < 7; i++) {
+    range.push({value: i, label: i})
+  }
+  return range
+}
