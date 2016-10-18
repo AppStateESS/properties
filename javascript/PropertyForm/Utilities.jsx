63d5a40319ee33be17ce349e523b92cd2bc26dec
Matthew McNaney <mcnaney@gmail.com>
2016-10-18 12:33:10 -0400

    Now contains utility information
    + Old code moved to UtilityImbursement

--- javascript/PropertyForm/Utilities.jsx
+++ javascript/PropertyForm/Utilities.jsx
@@ -1,88 +1,205 @@
 'use strict'
 import React from 'react'
-import InputField from '../Mixin/InputField.jsx'
-import Dollarize from '../Mixin/Dollarize.jsx'
+import bindMethods from '../Mixin/Bind.js'
+import ButtonGroup from '../Mixin/ButtonGroup.jsx'
 
 export default class Utilities extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
+    const methods = ['updateHeatType']
+    bindMethods(methods, this)
+  }
+
+  heatingTypes() {
+    return [
+      {
+        value: 1,
+        label: 'Heat pump'
+      }, {
+        value: 2,
+        label: 'Oil'
+      }, {
+        value: 3,
+        label: 'Propane'
+      }, {
+        value: 4,
+        label: 'Electric baseboard'
+      }, {
+        value: 5,
+        label: 'Kerosene'
+      }, {
+        value: 6,
+        label: 'Woodstove/Fireplace'
+      }, {
+        value: 7,
+        label: 'Natural gas'
+      }
+    ]
+  }
+
+  laundryTypes() {
+    return [
+      {
+        value: 0,
+        label: 'No laundry'
+      }, {
+        value: 1,
+        label: 'Washer/Dryer in unit'
+      }, {
+        value: 2,
+        label: 'Laundry room on premises'
+      }, {
+        value: 3,
+        label: 'Washer/Dryer hook ups in unit'
+      }
+    ]
+  }
+
+  trashTypes() {
+    return [
+      {
+        value: 0,
+        label: 'No pickup or bins'
+      }, {
+        value: 1,
+        label: 'Curbside pickup'
+      }, {
+        value: 2,
+        label: 'Trash only, no recycling bins'
+      }, {
+        value: 3,
+        label: 'Both bins on site'
+      }
+    ]
+  }
+
+  televisionTypes() {
+    return [
+      {
+        value: 0,
+        label: 'Antenna'
+      }, {
+        value: 1,
+        label: 'Cable'
+      }, {
+        value: 2,
+        label: 'Satellite'
+      }, {
+        value: 3,
+        label: 'Fiber'
+      }
+    ]
+  }
+
+  internetTypes() {
+    return [
+      {
+        value: 1,
+        label: 'Dial up'
+      }, {
+        value: 2,
+        label: 'DSL'
+      }, {
+        value: 3,
+        label: 'Wireless'
+      }, {
+        value: 4,
+        label: 'Satellite'
+      }, {
+        value: 5,
+        label: 'Cable'
+      }, {
+        value: 6,
+        label: 'DSL/Cable'
+      }, {
+        value: 7,
+        label: 'Fiber'
+      }
+    ]
+  }
+
+  updateHeatType(type) {
+    let heatType = this.props.property.heat_type
+
+    const index = heatType.indexOf(type)
+    if (index === -1) {
+      heatType.push(type)
+    } else {
+      heatType.splice(index, 1)
+    }
+    this.props.setValue('heat_type', heatType)
   }
 
   render() {
-    const property = this.props.property
+    const {property} = this.props
     return (
       <div>
-        <h3>Utility imbursement</h3>
-        <p>If you pay a portion of utilities, please enter that amount below.</p>
-        <div className="row">
-          <div className="col-sm-6">
-            <label>Cable</label>
-            <Dollarize>
-              <InputField
-                name="util_cable"
-                value={property.util_cable}
-                change={this.props.setValue.bind(this, 'util_cable')}/>
-            </Dollarize>
-          </div>
-          <div className="col-sm-6">
-            <label>Fuel/Gas</label>
-            <Dollarize>
-              <InputField
-                name="util_fuel"
-                value={property.util_fuel}
-                change={this.props.setValue.bind(this, 'util_fuel')}/>
-            </Dollarize>
+        <h3>Utilities</h3>
+        <div className="row bg-info">
+          <div className="col-sm-12">
+            <label>Heating</label>
+            <small>(Click all that apply)</small>
+            <ButtonGroup
+              name="heat_type"
+              multiple={true}
+              buttons={this.heatingTypes()}
+              match={property.heat_type}
+              handle={this.updateHeatType}
+              activeColor="success"/>
           </div>
         </div>
-        < div className="row">
-          <div className="col-sm-6">
+        <div className="row">
+          <div className="col-sm-12">
             <label>Internet</label>
-            <Dollarize>
-              <InputField
-                name="util_internet"
-                value={property.util_internet}
-                change={this.props.setValue.bind(this, 'util_internet')}/>
-            </Dollarize>
+            <ButtonGroup
+              name="internet_type"
+              buttons={this.internetTypes()}
+              match={property.internet_type}
+              handle={this.props.setIntegerValue.bind(this, 'internet_type')}
+              activeColor="success"/>
           </div>
-          < div className="col-sm-6">
-            <label>Phone</label>
-            < Dollarize >
-              <InputField
-                name="util_phone"
-                value={property.util_phone}
-                change={this.props.setValue.bind(this, 'util_phone')}/>
-              < /Dollarize></div >
-            </div>
-            < div className="row">
-              <div className="col-sm-6">
-                <label>Power</label>
-                <Dollarize>
-                  <InputField
-                    name="util_power"
-                    value={property.util_power}
-                    change={this.props.setValue.bind(this, 'util_power')}/>
-                </Dollarize>
-              </div>
-              < div className="col-sm-6">
-                <label>Trash</label>
-                < Dollarize >
-                  <InputField
-                    name="util_trash"
-                    value={property.util_trash}
-                    change={this.props.setValue.bind(this, 'util_trash')}/>
-                  < /Dollarize></div >
-                </div>
-                < div className="row">
-                  <div className="col-sm-6">
-                    <label>Water</label>
-                    <Dollarize>
-                      <InputField
-                        name="util_water"
-                        value={property.util_water}
-                        change={this.props.setValue.bind(this, 'util_water')}/>
-                    </Dollarize>
-                  </div>
-                  < /div></div >) } } Utilities.propTypes = {property : React.PropTypes.object,
-                  setValue : React.PropTypes.func
+        </div>
+        <div className="row bg-info">
+          <div className="col-sm-12">
+            <label>Laundry</label>
+            <ButtonGroup
+              name="laundry_type"
+              buttons={this.laundryTypes()}
+              match={property.laundry_type}
+              handle={this.props.setIntegerValue.bind(this, 'laundry_type')}
+              activeColor="success"/>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <label>Trash and Recycling</label>
+            <ButtonGroup
+              name="trash_type"
+              buttons={this.trashTypes()}
+              match={property.trash_type}
+              handle={this.props.setIntegerValue.bind(this, 'trash_type')}
+              activeColor="success"/>
+          </div>
+        </div>
+        <div className="row bg-info">
+          <div className="col-sm-12">
+            <label>Television</label>
+            <ButtonGroup
+              name="tv_type"
+              buttons={this.televisionTypes()}
+              match={property.tv_type}
+              handle={this.props.setIntegerValue.bind(this, 'tv_type')}
+              activeColor="success"/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Utilities.propTypes = {
+  property: React.PropTypes.object,
+  setIntegerValue: React.PropTypes.func,
+  setValue: React.PropTypes.func
 }
---
+++ javascript/PropertyForm/UtilityImbursement.jsx
@@ -0,0 +1,90 @@
+'use strict'
+import React from 'react'
+import InputField from '../Mixin/InputField.jsx'
+import Dollarize from '../Mixin/Dollarize.jsx'
+
+export default class UtilityImbursement extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    const property = this.props.property
+    return (
+      <div>
+        <h3>Utility imbursement</h3>
+        <p>If you pay a portion of utilities, please enter that amount below.</p>
+        <div className="row">
+          <div className="col-xs-6 col-sm-4 col-md-3">
+            <label>Cable</label>
+            <Dollarize>
+              <InputField
+                name="util_cable"
+                value={property.util_cable}
+                change={this.props.setValue.bind(this, 'util_cable')}/>
+            </Dollarize>
+          </div>
+          <div className="col-xs-6 col-sm-4 col-md-3">
+            <label>Fuel/Gas</label>
+            <Dollarize>
+              <InputField
+                name="util_fuel"
+                value={property.util_fuel}
+                change={this.props.setValue.bind(this, 'util_fuel')}/>
+            </Dollarize>
+          </div>
+          <div className="col-xs-6 col-sm-4 col-md-3">
+            <label>Internet</label>
+            <Dollarize>
+              <InputField
+                name="util_internet"
+                value={property.util_internet}
+                change={this.props.setValue.bind(this, 'util_internet')}/>
+            </Dollarize>
+          </div>
+          <div className="col-xs-6 col-sm-4 col-md-3">
+            <label>Phone</label>
+            <Dollarize>
+              <InputField
+                name="util_phone"
+                value={property.util_phone}
+                change={this.props.setValue.bind(this, 'util_phone')}/>
+            </Dollarize>
+          </div>
+          <div className="col-xs-6 col-sm-4 col-md-3">
+            <label>Power</label>
+            <Dollarize>
+              <InputField
+                name="util_power"
+                value={property.util_power}
+                change={this.props.setValue.bind(this, 'util_power')}/>
+            </Dollarize>
+          </div>
+          <div className="col-xs-6 col-sm-4 col-md-3">
+            <label>Trash</label>
+            <Dollarize>
+              <InputField
+                name="util_trash"
+                value={property.util_trash}
+                change={this.props.setValue.bind(this, 'util_trash')}/>
+            </Dollarize>
+          </div>
+          <div className="col-xs-6 col-sm-4 col-md-3">
+            <label>Water</label>
+            <Dollarize>
+              <InputField
+                name="util_water"
+                value={property.util_water}
+                change={this.props.setValue.bind(this, 'util_water')}/>
+            </Dollarize>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+UtilityImbursement.propTypes = {
+  property: React.PropTypes.object,
+  setValue: React.PropTypes.func
+}
