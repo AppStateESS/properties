6e0c81fa1ff11e9bcfe588470a6f40dbb36ace93
Matthew McNaney <mcnaney@gmail.com>
2016-11-01 15:36:39 -0400

    added missing parameters

--- class/Resource/Property.php
+++ class/Resource/Property.php
@@ -60,6 +60,10 @@ class Property extends Base
     protected $timeout;
     protected $trash_type;
     protected $updated;
+    protected $util_cable;
+    protected $util_fuel;
+    protected $util_internet;
+    protected $util_phone;
     protected $util_water;
     protected $util_trash;
     protected $util_power;
@@ -68,59 +72,60 @@ class Property extends Base
     protected $window_number;
     protected $workout_room;
     protected $table = 'properties';
+    protected $company_name;
 
     public function __construct()
     {
         parent::__construct();
         $this->active = new Variable\Bool(false, 'active');
-        $this->address = new Variable\String(null, 'address');
+        $this->address = new Variable\String('', 'address');
         $this->admin_fee_amt = new Variable\Integer(0, 'admin_fee_amt');
         $this->admin_fee_refund = new Variable\Bool(false, 'admin_fee_refund');
         $this->airconditioning = new Variable\Bool(false, 'airconditioning');
         $this->appalcart = new Variable\Bool(false, 'appalcart');
         $this->approved = new Variable\Bool(false, 'approved');
         $this->bathroom_no = new \phpws2\Variable\Decimal(1.0, 'bathroom_no');
-        $this->bedroom_no = new Variable\Integer(0, 'bedroom_no');
-        $this->bedroom_no->setRange(0, 100);
-        
+        $this->bedroom_no = new Variable\Integer(1, 'bedroom_no');
+        $this->bedroom_no->setRange(1, 10);
+
         $this->campus_distance = new Variable\Integer(0, 'campus_distance');
         $this->campus_distance->setRange(0, 100);
-        
+
         $this->clean_fee_amt = new Variable\Integer(0, 'clean_fee_amt');
         $this->clean_fee_refund = new Variable\Bool(0, 'clean_fee_refund');
         $this->clubhouse = new Variable\Bool(false, 'clubhouse');
         $this->contact_id = new Variable\Integer(0, 'contact_id');
         $this->contact_id->setRange(1);
-        $this->contract_length = new Variable\Integer(0, 'contract_length');
+        $this->contract_length = new Variable\Integer(3, 'contract_length');
         $this->created = new Variable\DateTime(null, 'created');
-        $this->description = new Variable\String(null, 'description');
+        $this->description = new Variable\String('', 'description');
         $this->dishwasher = new Variable\Bool(false, 'dishwasher');
         $this->efficiency = new Variable\Bool(false, 'efficiency');
         $this->furnished = new Variable\Bool(false, 'furnished');
-        $this->heat_type = new Variable\String(null, 'heat_type');
+        $this->heat_type = new Variable\String('', 'heat_type');
         $this->heat_type->setLimit(100);
         $this->internet_type = new Variable\Integer(0, 'internet_type');
         $this->internet_type->setRange(0, 20);
-        
+
         $this->lease_type = new Variable\Integer(0, 'lease_type');
         $this->lease_type->setRange(0, 10);
-        
+
         $this->laundry_type = new Variable\Integer(0, 'laundry_type');
         $this->laundry_type->setRange(0, 10);
-        
+
         $this->monthly_rent = new Variable\Integer(0, 'monthly_rent');
-        $this->move_in_date = new Variable\Integer(0, 'move_in_date');
-        $this->name = new Variable\String(null, 'name');
-        $this->name->setLimit(255);
-        
-        $this->other_fees = new Variable\String(null, 'other_fees');
-        $this->parking_fee = new Variable\Integer(0, 'parking_fee');
+        $this->move_in_date = new Variable\Date(0, 'move_in_date');
+        $this->name = new Variable\String('', 'name');
+        $this->name->setLimit(100);
+
+        $this->other_fees = new Variable\String('', 'other_fees');
+        $this->parking_fee = new Variable\Integer(1, 'parking_fee');
         $this->parking_per_unit = new Variable\Integer(0, 'parking_per_unit');
         $this->pet_deposit = new Variable\Integer(0, 'pet_deposit');
         $this->pet_dep_refund = new Variable\Bool(false, 'pet_dep_refund');
         $this->pets_allowed = new Variable\Bool(false, 'pets_allowed');
         $this->pet_fee = new Variable\Integer(0, 'pet_fee');
-        $this->pet_type = new Variable\String(null, 'pet_type');
+        $this->pet_type = new Variable\String('', 'pet_type');
         $this->security_amt = new Variable\Integer(0, 'security_amt');
         $this->security_refund = new Variable\Bool(false, 'security_refund');
         $this->student_type = new Variable\Integer(0, 'student_type');
@@ -128,15 +133,171 @@ class Property extends Base
         $this->timeout = new Variable\Integer(0, 'timeout');
         $this->trash_type = new Variable\Integer(0, 'trash_type');
         $this->trash_type->setRange(0, 10);
-        $this->updated = new Variable\DateTime(0, 'updated');
-        $this->util_water = new Variable\Bool(false, 'util_water');
-        $this->util_trash = new Variable\Bool(false, 'util_trash');
-        $this->util_power = new Variable\Bool(false, 'util_power');
+        $this->updated = new Variable\DateTime(time(), 'updated');
+        $this->util_water = new Variable\Integer(0, 'util_water');
+        $this->util_trash = new Variable\Integer(0, 'util_trash');
+        $this->util_power = new Variable\Integer(0, 'util_power');
+        $this->util_cable = new Variable\Integer(0, 'util_cable');
+        $this->util_fuel = new Variable\Integer(0, 'util_fuel');
+        $this->util_internet = new Variable\Integer(0, 'util_internet');
+        $this->util_phone = new Variable\Integer(0, 'util_phone');
         $this->utilities_inc = new Variable\Bool(false, 'utilities_inc');
         $this->tv_type = new Variable\Integer(0, 'tv_type');
         $this->tv_type->setRange(0, 10);
         $this->window_number = new Variable\Bool(true, 'window_number');
         $this->workout_room = new Variable\Bool(false, 'workout_room');
+
+        $this->company_name = new Variable\String('', 'company_name');
+    }
+
+    public function getCampusDistance()
+    {
+        switch ($this->campus_distance->get()) {
+            case '0':
+                return 'Five miles or less';
+            case '5':
+                return 'Between five and ten miles';
+            case '10':
+                return 'Between 10 and 25 miles';
+            case '25':
+                return '25 miles or more';
+
+            default:
+                return false;
+        }
+    }
+
+    public function getContractLength()
+    {
+        switch ($this->contract_length->get()) {
+            case '1':
+                return 'Monthly';
+
+            case '2':
+                return 'Every six months';
+
+            case '3':
+                return 'Yearly (12 months)';
+
+            case '4':
+                return 'Summer only';
+
+            case '5':
+                return 'Per semester';
+
+            case '6':
+                return 'Two semesters, no summer';
+
+            case '7':
+                return '10 months';
+
+            case '8':
+                return '5 months';
+
+            default:
+                return false;
+        }
+    }
+
+    public function getInternetType()
+    {
+        switch ($this->internet_type->get()) {
+            case '1':
+                return 'Dial up only';
+            case '2':
+                return 'DSL';
+            case '3':
+                return 'Wireless';
+            case '4':
+                return 'Satellite';
+            case '5':
+                return 'Cable';
+            case '6':
+                return 'DSL/Cable';
+            case '7':
+                return 'Fiber';
+        }
+    }
+
+    public function getLaundryType()
+    {
+        switch ($this->laundry_type->get()) {
+            case '0':
+                return 'No laundry';
+            case '1':
+                return 'Washer/Dryer in unit';
+            case '2':
+                return 'Laundry room on premises';
+            case '3':
+                return 'Washer/Dryer hook ups in unit';
+        }
+        return false;
+    }
+
+    public function getMoveInDate()
+    {
+        return $this->move_in_date->__toString();
+    }
+
+    public function getStudentType()
+    {
+        switch ($this->student_type->get()) {
+            case '0':
+                return 'No preference';
+
+            case '1':
+                return 'Undergraduate';
+
+            case '2':
+                return 'Graduate';
+        }
+    }
+
+    public function getTrashType()
+    {
+        switch ($this->trash_type) {
+            case '0':
+                return 'No pickup or bins';
+            case '1':
+                return 'Curbside pickup';
+            case '2':
+                return 'Trash only, no recycling bins';
+            case '3':
+                return 'Both bins on site';
+        }
+    }
+
+    public function getTvType()
+    {
+        switch ($this->tv_type) {
+            case '0':
+                return 'Antenna';
+            case '1':
+                return 'Cable';
+            case '2':
+                return 'Satellite';
+            case '3':
+                return 'Fiber';
+        }
+    }
+
+    public function view()
+    {
+        $view = $this->getStringVars();
+        $view['campus_distance'] = $this->getCampusDistance();
+        $view['contract_length'] = $this->getContractLength();
+        $view['internet_type'] = $this->getInternetType();
+        $view['lease_type'] = $this->lease_type === '0' ? 'per unit' : 'per renter';
+        $view['laundry_type'] = $this->getLaundryType();
+        $view['move_in_date'] = $this->getMoveInDate();
+        $view['trash_type'] = $this->getTrashType();
+        $view['admin_fee_refund'] = $this->admin_fee_refund->get() ? 'Refundable' : 'Non-refundable';
+        $view['pet_dep_refund'] = $this->pet_dep_refund->get() ? 'Refundable' : 'Non-refundable';
+        $view['security_refund'] = $this->security_refund->get() ? 'Refundable' : 'Non-refundable';
+        $view['student_type'] = $this->getStudentType();
+        $view['tv_type'] = $this->getTvType();
+
+        return $view;
     }
 
 }
