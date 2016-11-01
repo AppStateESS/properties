5f1e3016053206ab34727b922deaace58fb7c1bd
Matthew McNaney <mcnaney@gmail.com>
2016-11-01 15:44:36 -0400

    tidy

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,31 @@ var webpack = require('webpack')
 var Promise = require('es6-promise').polyfill()
 
 module.exports = {
-  entry : setup.entry,
+  entry: setup.entry,
   output: {
     path: setup.path.join(setup.APP_DIR, "dev"),
     filename: "[name].js"
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin("vendor","vendor.js")
-  ],
+  plugins: [new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.js")],
   module: {
-    preLoaders: [{
-      test: /\.jsx?$/,
-      loader: 'jshint-loader',
-      exclude: '/node_modules/',
-      include: setup.APP_DIR + "/dev"
-    }],
-    loaders: [{
-      test: /\.jsx?/,
-      include: setup.APP_DIR,
-      loader: 'babel'
-    }, {
-      test: /\.css$/,
-      loader: "style-loader!css-loader"
-    }]
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'jshint-loader',
+        exclude: '/node_modules/',
+        include: setup.APP_DIR + "/dev"
+      }
+    ],
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: setup.APP_DIR,
+        loader: 'babel'
+      }, {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
   },
   devtool: 'source-map'
 }
