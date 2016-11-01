c04b6d4f201a5b3ad6ca87896a3964d2eb6176e7
Matthew McNaney <mcnaney@gmail.com>
2016-11-01 14:41:57 -0400

    Role moved into Base factory

--- class/Factory/BaseFactory.php
+++ class/Factory/BaseFactory.php
@@ -2,6 +2,8 @@
 
 namespace properties\Factory;
 
+use properties\Exception\ResourceNotFound;
+
 /**
  *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
@@ -9,7 +11,32 @@ namespace properties\Factory;
 abstract class BaseFactory extends \phpws2\ResourceFactory
 {
 
+    /**
+     *
+     * @var \properties\Role\BaseRole
+     */
+    public $role;
+
     abstract protected function build();
+
+    public function __construct($controller)
+    {
+        $request = \Server::getCurrentRequest();
+        $this->loadRole($controller, $request->getMethod());
+    }
+
+    protected function loadRole($controller, $method)
+    {
+        if (\Current_User::allow('properties')) {
+            $this->role = new \properties\Role\AdminRole($controller, $method);
+        } elseif ((bool) isset($_SESSION['Contact_Manager']) && $_SESSION['Contact_Manager']->id) {
+            $this->role = new \properties\Role\ManagerRole($controller, $method);
+        } elseif (\Current_User::isLogged()) {
+            $this->role = new \properties\Role\LoggedRole($controller, $method);
+        } else {
+            $this->role = new \properties\Role\UserRole($controller, $method);
+        }
+    }
 
     public function load($id)
     {
@@ -19,7 +46,7 @@ abstract class BaseFactory extends \phpws2\ResourceFactory
         }
         $resource->setId($id);
         if (!parent::loadByID($resource)) {
-            throw new \Exception('Resource id not found:' . $id);
+            throw new ResourceNotFound($id);
         }
         return $resource;
     }
