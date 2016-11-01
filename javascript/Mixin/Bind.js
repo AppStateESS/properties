26872ff3d72cad5e0aa1a21a76d8ac171ec18490
Matthew McNaney <mcnaney@gmail.com>
2016-11-01 15:39:36 -0400

    Added error message

--- javascript/Mixin/Bind.js
+++ javascript/Mixin/Bind.js
@@ -1,5 +1,8 @@
 export default function bindMethods(bindable, object) {
   bindable.map(function (v) {
+    if (object[v] === undefined) {
+      throw new Error(`Cannot bind undefined method: ${v}`)
+    }
     object[v] = this[v].bind(object)
   }.bind(object))
 }
