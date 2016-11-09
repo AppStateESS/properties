0c432a01e31a66205d4a0b7a32eda22c5b54ca43
Matthew McNaney <mcnaney@gmail.com>
2016-11-09 12:14:27 -0500

    Value keys changed to string. Overall easier to support just one type.

--- javascript/PropertyForm/Utilities.jsx
+++ javascript/PropertyForm/Utilities.jsx
@@ -15,25 +15,25 @@ export default class Utilities extends React.Component {
   heatingTypes() {
     return [
       {
-        value: 1,
+        value: '1',
         label: 'Heat pump'
       }, {
-        value: 2,
+        value: '2',
         label: 'Oil'
       }, {
-        value: 3,
+        value: '3',
         label: 'Propane'
       }, {
-        value: 4,
+        value: '4',
         label: 'Electric baseboard'
       }, {
-        value: 5,
+        value: '5',
         label: 'Kerosene'
       }, {
-        value: 6,
+        value: '6',
         label: 'Woodstove/Fireplace'
       }, {
-        value: 7,
+        value: '7',
         label: 'Natural gas'
       }
     ]
@@ -42,16 +42,16 @@ export default class Utilities extends React.Component {
   laundryTypes() {
     return [
       {
-        value: 0,
+        value: '0',
         label: 'No laundry'
       }, {
-        value: 1,
+        value: '1',
         label: 'Washer/Dryer in unit'
       }, {
-        value: 2,
+        value: '2',
         label: 'Laundry room on premises'
       }, {
-        value: 3,
+        value: '3',
         label: 'Washer/Dryer hook ups in unit'
       }
     ]
@@ -60,16 +60,16 @@ export default class Utilities extends React.Component {
   trashTypes() {
     return [
       {
-        value: 0,
+        value: '0',
         label: 'No pickup or bins'
       }, {
-        value: 1,
+        value: '1',
         label: 'Curbside pickup'
       }, {
-        value: 2,
+        value: '2',
         label: 'Trash only, no recycling bins'
       }, {
-        value: 3,
+        value: '3',
         label: 'Both bins on site'
       }
     ]
@@ -78,16 +78,16 @@ export default class Utilities extends React.Component {
   televisionTypes() {
     return [
       {
-        value: 0,
+        value: '0',
         label: 'Antenna'
       }, {
-        value: 1,
+        value: '1',
         label: 'Cable'
       }, {
-        value: 2,
+        value: '2',
         label: 'Satellite'
       }, {
-        value: 3,
+        value: '3',
         label: 'Fiber'
       }
     ]
@@ -96,25 +96,25 @@ export default class Utilities extends React.Component {
   internetTypes() {
     return [
       {
-        value: 1,
+        value: '1',
         label: 'Dial up'
       }, {
-        value: 2,
+        value: '2',
         label: 'DSL'
       }, {
-        value: 3,
+        value: '3',
         label: 'Wireless'
       }, {
-        value: 4,
+        value: '4',
         label: 'Satellite'
       }, {
-        value: 5,
+        value: '5',
         label: 'Cable'
       }, {
-        value: 6,
+        value: '6',
         label: 'DSL/Cable'
       }, {
-        value: 7,
+        value: '7',
         label: 'Fiber'
       }
     ]
@@ -122,7 +122,6 @@ export default class Utilities extends React.Component {
 
   updateHeatType(type) {
     let heatType = this.props.property.heat_type
-
     const index = heatType.indexOf(type)
     if (index === -1) {
       heatType.push(type)
