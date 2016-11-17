fe27e115c62376bd9817e620a9faa08664fe9454
Matthew McNaney <mcnaney@gmail.com>
2016-11-17 12:22:13 -0500

    Can set main photo. Small alyout edits.

--- javascript/PropertyImage/Thumb.jsx
+++ javascript/PropertyImage/Thumb.jsx
@@ -4,17 +4,25 @@ import React from 'react'
 export default class Thumb extends React.Component {
   constructor(props) {
     super(props)
+    this.delete = this.delete.bind(this)
+  }
+
+  delete(e) {
+    e.preventDefault()
+    this.props.delete()
   }
 
   render() {
     const outer = {
-      width: '180px',
-      height: '180px',
+      width: '184px',
+      height: '184px',
       float: 'left',
       margin: '0px 8px 8px 0',
       textAlign: 'center',
       backgroundColor: '#e3e3e3',
-      border: '1px solid #bbb',
+      borderWidth: '2px',
+      borderStyle: 'solid',
+      borderColor: this.props.main_pic === '1' ? 'blue' : '#bbb',
       position: 'relative'
     }
 
@@ -22,13 +30,22 @@ export default class Thumb extends React.Component {
       position: 'absolute',
       display: 'block',
       bottom: '0px',
-      left: '82px',
+      left: '72px',
       cursor: 'pointer'
     }
+
+    const main = {
+      position : 'absolute',
+      top: '0px',
+      width: '100%',
+      backgroundColor: 'rgba(0,0,255,0.4)',
+      color : 'white'
+    }
     return (
-      <div style={outer}>
+      <div style={outer} onClick={this.props.setMain}>
+        {this.props.main_pic === '1' ? <div style={main}>Main photo</div>:null}
         <img src={this.props.thumbnail}/>
-        <span className="fa-stack fa-lg" style={inner} onClick={this.props.delete}>
+        <span className="fa-stack fa-lg" style={inner} onClick={this.delete}>
           <i className="text-danger fa fa-circle fa-stack-2x"></i>
           <i className="text-danger fa fa-trash-o fa-stack-1x fa-inverse"></i>
         </span>
@@ -39,6 +56,12 @@ export default class Thumb extends React.Component {
 
 Thumb.propTypes = {
   thumbnail: React.PropTypes.string,
+  main_pic : React.PropTypes.string,
   id : React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]),
-  delete: React.PropTypes.func
+  delete: React.PropTypes.func,
+  setMain: React.PropTypes.func,
+}
+
+Thumb.defaultProps = {
+  main : false
 }
