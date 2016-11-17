73c5a719a112b5f23d4c5c87db2ad9eb6f75b52c
Matthew McNaney <mcnaney@gmail.com>
2016-11-17 12:10:24 -0500

    Layout changed

--- javascript/Manager/ManagerRow.jsx
+++ javascript/Manager/ManagerRow.jsx
@@ -58,14 +58,12 @@ class ManagerRow extends React.Component {
   }
 
   render() {
-    //let lastLog = 'Never'
+    let lastLog
 
-    /*
-    if (this.props.last_log > 0) {
+    if (this.props.last_log !== null) {
       let lastDate = new Date(this.props.last_log * 1000)
       lastLog = lastDate.toDateString()
     }
-    */
 
     let alabel = null
     let aicon = null
@@ -95,11 +93,10 @@ class ManagerRow extends React.Component {
           icon: <i className="fa fa-pencil-square-o"></i>,
           handleClick: this.props.fillForm
         }, {
-          label : 'Add property',
+          label: 'Add property',
           icon: <i className="fa fa-building-o"></i>,
           link: './properties/Property/create/?managerId=' + this.props.id
-        },
-        {
+        }, {
           label: alabel,
           icon: aicon,
           handleClick: ahandle
@@ -113,44 +110,43 @@ class ManagerRow extends React.Component {
     }
 
     let properties = <div>
-      <em>No properties</em>
+      <em>None</em>
     </div>
     if (this.props.property_count > 0) {
       properties = <a
-        href="./properties/Property/?managerId={this.props.id}"
+        href={`./properties/Property/?managerId=${this.props.id}`}
         className="btn btn-default">View Properties</a>
     }
 
     let co = null
     if (this.props.first_name.length > 0) {
-      co = <span>(c/o {this.props.first_name}&nbsp;{this.props.last_name})</span>
+      co = <small>(c/o {this.props.first_name}&nbsp;{this.props.last_name})</small>
     }
     const email = 'mailto:' + this.props.email_address
     const phone = 'tel:+1' + this.props.phone.replace(/[^\d]/g, '')
 
     return (
-      <div
-        className={this.props.active === '0'
-        ? 'bg-danger row manager-row'
-        : 'row manager-row'}>
-        <div className="col-sm-3">
+      <tr className={this.props.active === '0'
+        ? 'bg-danger'
+        : ''}>
+        <td>
           <span className="company-name">{this.props.company_name}</span><br/>{co}
-        </div>
-        <div className="col-sm-3">
+        </td>
+        <td>
           {properties}
-        </div>
-        <div className="col-sm-4">
+        </td>
+        <td>
           <LinkToButton url={phone} icon="fa-phone" label={this.props.phone}/>
           <Website url={this.props.company_url}/>
           <LinkToButton
             url={email}
             icon="fa-envelope-o"
             label={this.props.email_address}/>
-        </div>
-        {this.props.admin
-          ? <div className="col-sm-2">{optionList}</div>
+        </td>
+        {this.props.admin === true
+          ? <td>{optionList}</td>
           : null}
-      </div>
+      </tr>
     )
   }
 }
@@ -158,7 +154,7 @@ class ManagerRow extends React.Component {
 ManagerRow.defaultProps = {
   phone: '',
   email_address: '',
-  last_log: 0,
+  last_log: null,
   company_url: '',
   first_name: '',
   last_name: '',
@@ -166,28 +162,28 @@ ManagerRow.defaultProps = {
 }
 
 ManagerRow.propTypes = {
-  id: React.PropTypes.string,
-  phone: React.PropTypes.string,
+  property_count: React.PropTypes.string,
   email_address: React.PropTypes.string,
   company_name: React.PropTypes.string,
+  showProperties: React.PropTypes.func,
   company_url: React.PropTypes.string,
   first_name: React.PropTypes.string,
   last_name: React.PropTypes.string,
   last_log: React.PropTypes.string,
-  property_count: React.PropTypes.string,
-  active: React.PropTypes.string,
   fillForm: React.PropTypes.func,
+  active: React.PropTypes.string,
+  message: React.PropTypes.func,
+  phone: React.PropTypes.string,
   remove: React.PropTypes.func,
   reload: React.PropTypes.func,
-  showProperties: React.PropTypes.func,
-  message: React.PropTypes.func,
-  admin: React.PropTypes.bool
+  admin: React.PropTypes.bool,
+  id: React.PropTypes.string
 }
 
 class Website extends React.Component {
   render() {
     if (this.props.url.length > 0) {
-      return (<LinkToButton url={this.props.url} label={this.props.url} icon="fa-link"/>)
+      return (<LinkToButton url={this.props.url} label="Website" icon="fa-link"/>)
     } else {
       return null
     }
@@ -204,10 +200,16 @@ class LinkToButton extends React.Component {
     const smallIconClass = 'fa ' + this.props.icon
     return (
       <span>
-        <a href={this.props.url} className="link-button visible-xs-inline-block btn btn-primary">
+        <a
+          href={this.props.url}
+          target="_blank"
+          className="link-button visible-xs-inline-block btn btn-primary">
           <i className={bigIconClass}></i>
         </a>
-        <a href={this.props.url} className="visible-sm visible-md visible-lg">
+        <a
+          href={this.props.url}
+          className="visible-sm visible-md visible-lg"
+          target="_blank">
           <i className={smallIconClass}></i>&nbsp;{this.props.label}
         </a>
       </span>
