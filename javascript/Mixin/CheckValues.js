d2e59588375ae610142c65d9d5a4c5e33bfa274b
Matthew McNaney <mcnaney@gmail.com>
2016-12-01 15:32:59 -0500

    lint fixes

--- javascript/Mixin/CheckValues.js
+++ javascript/Mixin/CheckValues.js
@@ -1,20 +1,19 @@
 class CheckValues {
-    static isEmpty(value) {
-        return value === '' || value === null;
-    }
+  static isEmpty(value) {
+    return value === '' || value === null
+  }
 
-    static isEmail(value) {
-        return value.match(/^[\w.%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i);
-    }
+  static isEmail(value) {
+    return value.match(/^[\w.%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i)
+  }
 
-    static isPhone(value) {
-        return (value.replace(/[^\d]/, '').length >= 7);
-    }
+  static isPhone(value) {
+    return (value.replace(/[^\d]/g, '').length == 10)
+  }
 
-    static randomId() {
-        return (Math.random().toString(36) + '00000000000000000').slice(2,
-            10);
-    }
+  static randomId() {
+    return (Math.random().toString(36) + '00000000000000000').slice(2, 10)
+  }
 }
 
-export default CheckValues;
+export default CheckValues
