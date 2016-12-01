2b499b9b0f772ebe7bc51f828eef48f331ad72ea
Matthew McNaney <mcnaney@gmail.com>
2016-12-01 15:28:21 -0500

    ManagerFactory now child of abstract
    + I needed a separate factory for signup, so created
    base abstract for each to use. Allowed separation of
    roles as well.

--- class/Factory/ManagerFactory.php
+++ class/Factory/ManagerFactory.php
@@ -14,25 +14,14 @@ use properties\Exception\MissingInput;
 use properties\Exception\PrivilegeMissing;
 
 /**
- * Description of ManagerFactory
- *
  * @author Matthew McNaney <mcnaneym@appstate.edu>
  */
-class ManagerFactory extends BaseFactory
+class ManagerFactory extends ManagerFactoryAbstract
 {
 
     public function __construct()
     {
         parent::__construct('manager');
-    }
-
-    /**
-     * 
-     * @return properties\Resource\Manager
-     */
-    public function build()
-    {
-        return new Resource;
     }
 
     public function listing($limit = 100, $search = null)
@@ -102,213 +91,26 @@ class ManagerFactory extends BaseFactory
     }
 
     /**
-     * 
-     * @param integer $id
-     * @return \properties\Resource\Manager
-     */
-    public function load($id)
-    {
-        $manager = parent::load($id);
-        $db = Database::getDB();
-        $db->addTable('properties')->addField('id', 'count')->showCount();
-        $manager->property_count = $db->selectColumn();
-        return $manager;
-    }
-
-    /**
      * Administrative post of the manager
      * @return string
      */
-    public function post(\Request $request)
+    public function adminPost(\Request $request)
     {
-        $errors = array();
-        $r = new Resource;
+        $manager = $this->loadResourceFromPost($request);
+        // if array, there were problems
+        if (is_array($manager)) {
+            return $manager;
+        }
         try {
-            $id = $request->pullPostInteger('id', true);
-            if ($id) {
-                $r->setId($id);
-            }
-            $username = strtolower($request->pullPostString('username', true));
-
-            if (empty($username)) {
-                $errors['usernameEmpty'] = true;
-            } elseif ($this->checkUsername($username, $id)) {
-                $errors['usernameDuplicate'] = true;
-            } else {
-                $r->username = $username;
-            }
-
-            $password = $request->pullPostString('password', true);
-
-            if (empty($password)) {
-                if ($id == 0) {
-                    $errors['passwordEmpty'] = true;
-                }
-            } else {
-                if (strlen($password) < 8) {
-                    $errors['passwordShort'] = true;
-                } else {
-                    $r->password = $password;
-                }
-            }
-
-            $first_name = $request->pullPostString('first_name', true);
-
-            if (empty($first_name)) {
-                $errors['firstNameEmpty'] = true;
-            } else {
-                $r->first_name = $first_name;
-            }
-
-            $last_name = $request->pullPostString('last_name', true);
-
-            if (empty($last_name)) {
-                $errors['lastNameEmpty'] = true;
-            } else {
-                $r->last_name = $last_name;
-            }
-
-            $phone = preg_replace('/[^\d]/', '',
-                    $request->pullPostString('phone', true));
-            if (empty($phone)) {
-                $errors['phoneEmpty'] = true;
-            } elseif (strlen($phone < 7)) {
-                $errors['phoneBadFormat'] = true;
-            } else {
-                $r->phone = $phone;
-            }
-
-            $email_address = strtolower($request->pullPostString('email_address',
-                            true));
-
-            if (empty($email_address)) {
-                $errors['emailEmpty'] = true;
-            } else {
-                try {
-                    $r->email_address = $email_address;
-                } catch (\Exception $e) {
-                    $errors['emailBadFormat'] = true;
-                }
-            }
-
-            $company_name = $request->pullPostString('company_name', true);
-            if (empty($company_name)) {
-                $errors['companyEmpty'] = true;
-            } elseif ($this->checkCompanyName($company_name, $id)) {
-                $errors['companyDuplicate'] = true;
-            } else {
-                $r->company_name = $company_name;
-            }
-
-            if (!empty($errors)) {
-                $json = $errors;
-                $json['status'] = 'error';
-                return $json;
-            }
-
-            $company_address = $request->pullPostString('company_address', true);
-            $r->company_address = $company_address === false ? null : $company_address;
-
-            $company_url = $request->pullPostString('company_url', true);
-            $r->company_url = $company_url === false ? null : $company_url;
-            
-            $times_available = $request->pullPostString('times_available',
-                    true);
-            $r->times_available = $times_available === false ? null : $times_available;
-
-            $this->saveResource($r);
-            $json = array('status' => 'success');
-        } catch (\properties\Exception\MissingInput $e) {
-            $json = array('status' => 'usererror', 'error' => $e->getMessage());
+            $manager->active = true;
+            $manager->approved = true;
+            $this->saveResource($manager);
         } catch (\Exception $e) {
             \phpws2\Error::log($e);
             $json = array('status' => 'fail', 'error' => 'An unrecoverable error occurred.');
         }
-
+        $json = array('status' => 'success');
         return $json;
-    }
-
-    /**
-     * Returns true if user name is already in use
-     * @param string $username
-     * @param integer $id
-     * @return boolean
-     */
-    public function checkUsername($username, $id = 0)
-    {
-        $db = Database::getDB();
-        $tbl = $db->addTable('prop_contacts');
-        $usernameCheck = $db->createConditional($tbl->getField('username'),
-                strtolower($username), 'like');
-        $passwordCheck = $db->createConditional($tbl->getField('email_address'),
-                strtolower($username), 'like');
-        $combined = $db->createConditional($usernameCheck, $passwordCheck, 'or');
-        if ($id > 0) {
-            $idCheck = $db->createConditional($tbl->getField('id'), $id, '!=');
-            $together = $db->createConditional($combined, $idCheck, 'and');
-            $db->addConditional($together);
-        } else {
-            $db->addConditional($combined);
-        }
-        $tbl->addField('id');
-        $result = $db->selectOneRow();
-        return (bool) $result;
-    }
-
-    /**
-     * Returns true if email is already in use
-     * @param string $email
-     * @param integer $id
-     * @return boolean
-     */
-    public function checkEmail($email, $id = 0)
-    {
-        $db = Database::getDB();
-        $tbl = $db->addTable('prop_contacts');
-        $email = $tbl->createConditional('email_address', strtolower($email),
-                'like');
-        if ($id > 0) {
-            $idCheck = $db->createConditional($tbl->getField('id'), $id, '!=');
-            $together = $db->createConditional($email, $idCheck, 'and');
-            $db->addConditional($together);
-        } else {
-            $db->addConditional($email);
-        }
-        $tbl->addField('id');
-        $result = $db->selectOneRow();
-        return (bool) $result;
-    }
-
-    /**
-     * Returns true if company name is in use
-     * @param string $company_name
-     * @param integer $id
-     * @return boolean
-     */
-    public function checkCompanyName($company_name, $id = 0)
-    {
-        $db = Database::getDB();
-        $tbl = $db->addTable('prop_contacts');
-        $company = $db->createConditional('company_name',
-                strtolower($company_name), 'like');
-        if ($id > 0) {
-            $idCheck = $db->createConditional($tbl->getField('id'), $id, '!=');
-            $together = $db->createConditional($company, $idCheck, 'and');
-            $db->addConditional($together);
-        } else {
-            $db->addConditional($company);
-        }
-        $tbl->addField('id');
-        $result = $db->selectOneRow();
-        return (bool) $result;
-    }
-
-    public function getProperties($contact_id)
-    {
-        $db = \phpws2\Database::getDB();
-        $tbl = $db->addTable('properties');
-        $tbl->addFieldConditional('contact_id', $contact_id);
-        return $db->select();
     }
 
     public function delete($id)
