b508f17afe3f50576290ce8ae3483c32a7dd8e88
Matthew McNaney <mcnaney@gmail.com>
2017-01-24 13:37:34 -0500

    Sublease added. Not functional yet.

---
+++ class/Factory/Sublease/Listing.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * See docs/AUTHORS and docs/COPYRIGHT for relevant info.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @author Matthew McNaney <mcnaney at gmail dot com>
+ *
+ * @license http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+class Listing
+{
+
+    public $search_string;
+    public $limit = 100;
+    public $beds = 1;
+    public $baths = 1;
+    public $minprice = 0;
+    public $maxprice = 0;
+    public $furnished = 0;
+    public $pets = 0;
+    public $appalcart = 0;
+    public $campus = 0; // within 5 miles of campus
+    public $utils = 0; // utilities comped
+    public $airconditioning = 0;
+    public $dishwasher = 0;
+    public $laundry = 0; // washer/dryer in unit
+    public $clubhouse = 0;
+    public $workout = 0;
+    public $efficiency = 0;
+    public $apartment = 0;
+    public $house = 0;
+    public $condo = 0;
+    public $townhouse = 0;
+    public $duplex = 0;
+
+}
