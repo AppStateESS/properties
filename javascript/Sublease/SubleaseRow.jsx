f431322f686fadf44cd83770f032beff661415e2
Matthew McNaney <mcnaney@gmail.com>
2017-01-27 10:46:02 -0500

    fleshed out

--- javascript/Sublease/SubleaseRow.jsx
+++ javascript/Sublease/SubleaseRow.jsx
@@ -20,10 +20,28 @@ export default class SubleaseRow extends ViewRow {
   render() {
     const {sublease} = this.props
     const link = `./properties/Sublease/${sublease.id}/${this.urlTitle(sublease.name)}`
-    return(
-      <div className="row property-row">
+    return (
+      <div className="row sublease-row">
         <div className="title">
           <a href={link}>{sublease.name}</a>
+        </div>
+        <div className="col-sm-4">
+          <div className="rent">{this.getRent()}</div>
+          <div className="room-bath">{sublease.proptype}&nbsp; - {sublease.bedroom_no}&nbsp;Bed, {sublease.bathroom_no}&nbsp;Bath
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className="availability"><strong>Availability:</strong> {sublease.move_in_date}</div>
+          <div className="end-date"><strong>Sublease end date:</strong> {sublease.move_out_date}</div>
+        </div>
+        <div className="col-sm-4">
+          {this.petsAllowed(sublease.pets_allowed)}
+          {this.furnished(sublease.furnished)}
+          {this.airconditioner(sublease.airconditioning)}
+          {this.dishwasher(sublease.dishwasher)}
+          {this.utilities(sublease.utilities_inc)}
+          {this.appalcart(sublease.appalcart)}
+          {this.washer(sublease.washer)}
         </div>
       </div>
     )
@@ -31,5 +49,5 @@ export default class SubleaseRow extends ViewRow {
 }
 
 SubleaseRow.propTypes = {
-  sublease : React.PropTypes.object
+  sublease: React.PropTypes.object
 }
