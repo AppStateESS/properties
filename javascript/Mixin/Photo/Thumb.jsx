6afa78c4b760463808c7da3ffb1246a6676424bd
Matthew McNaney <mcnaney@gmail.com>
2017-03-06 15:30:07 -0500

    Listing photos with li

--- javascript/Mixin/Photo/Thumb.jsx
+++ javascript/Mixin/Photo/Thumb.jsx
@@ -17,13 +17,14 @@ export default class Thumb extends React.Component {
     const outer = {
       width: '184px',
       height: '184px',
-      float: 'left',
       margin: '0px 8px 8px 0',
       textAlign: 'center',
       backgroundColor: '#e3e3e3',
       borderWidth: '2px',
       borderStyle: 'solid',
-      borderColor: this.props.main_pic === '1' ? 'blue' : '#bbb',
+      borderColor: this.props.main_pic === '1'
+        ? 'blue'
+        : '#bbb',
       position: 'relative'
     }
 
@@ -36,33 +37,43 @@ export default class Thumb extends React.Component {
     }
 
     const main = {
-      position : 'absolute',
+      position: 'absolute',
       top: '0px',
       width: '100%',
       backgroundColor: 'rgba(0,0,255,0.4)',
-      color : 'white'
+      color: 'white'
+    }
+
+    const listStyle = {
+      listStyleType : 'none',
+      display: 'inline',
+      float: 'left',
     }
     return (
-      <div style={outer} onClick={this.props.setMain}>
-        {this.props.main_pic === '1' ? <div style={main}>Main photo</div>:null}
-        <img src={this.props.thumbnail}/>
-        <span className="fa-stack fa-lg" style={inner} onClick={this.delete}>
-          <i className="text-danger fa fa-circle fa-stack-2x"></i>
-          <i className="text-danger fa fa-trash-o fa-stack-1x fa-inverse"></i>
-        </span>
-      </div>
+      <li style={listStyle}>
+        <div style={outer} onClick={this.props.setMain}>
+          {this.props.main_pic === '1'
+            ? <div style={main}>Main photo</div>
+            : null}
+          <img src={this.props.thumbnail}/>
+          <span className="fa-stack fa-lg" style={inner} onClick={this.delete}>
+            <i className="text-danger fa fa-circle fa-stack-2x"></i>
+            <i className="text-danger fa fa-trash-o fa-stack-1x fa-inverse"></i>
+          </span>
+        </div>
+      </li>
     )
   }
 }
 
 Thumb.propTypes = {
   thumbnail: React.PropTypes.string,
-  main_pic : React.PropTypes.string,
-  id : React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]),
+  main_pic: React.PropTypes.string,
+  id: React.PropTypes.oneOfType([React.PropTypes.number, React.PropTypes.string]),
   delete: React.PropTypes.func,
-  setMain: React.PropTypes.func,
+  setMain: React.PropTypes.func
 }
 
 Thumb.defaultProps = {
-  main : false
+  main: false
 }
