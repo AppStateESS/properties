d4f83dd310440c213427c3d4e60c1d8a894a13bc
Matthew McNaney <mcnaney@gmail.com>
2017-04-26 16:43:22 -0400

    updated to work with new babel/webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,27 +4,32 @@ var WebpackStripLoader = require('strip-loader')
 var Promise = require('es6-promise').polyfill()
 
 module.exports = {
-  entry : setup.entry,
+  entry: setup.entry,
   output: {
     path: setup.path.join(setup.APP_DIR, "build"),
     filename: "[name].js"
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin("vendor","vendor.js")
-  ],
+  plugins: [new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js'})],
   module: {
-    loaders: [{
-      test: /\.jsx?/,
-      include: setup.APP_DIR,
-      loader: 'babel'
-    }, {
-      test: [/\.js$/, /\.es6$/, /\.jsx$/],
-      exclude: /node_modules/,
-      loader: WebpackStripLoader.loader('console.log')
-    }, {
-      test: /\.css$/,
-      loader: "style-loader!css-loader"
-    }]
+    loaders: [
+      {
+        test: /\.jsx?/,
+        include: setup.APP_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }, {
+        test: [
+          /\.js$/, /\.es6$/, /\.jsx$/
+        ],
+        exclude: /node_modules/,
+        loader: WebpackStripLoader.loader('console.log')
+      }, {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
