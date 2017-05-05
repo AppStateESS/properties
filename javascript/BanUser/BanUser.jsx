e6952702c1d949a8c6a3010ebc5472875a942f93
Matthew McNaney <mcnaney@gmail.com>
2017-05-05 13:07:34 -0400

    Increase z-index on modal

--- javascript/BanUser/BanUser.jsx
+++ javascript/BanUser/BanUser.jsx
@@ -12,7 +12,8 @@ const modalStyle = {
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(93, 93, 93, 0.75)'
+    backgroundColor: 'rgba(93, 93, 93, 0.75)',
+    zIndex : 100
   },
   content: {
     position: 'absolute',
@@ -97,7 +98,6 @@ export default class BanUser extends Component {
             </div>
           </div>
         </Modal>
-        <button className="btn btn-default" onClick={this.openModal}>Modal</button>
       </div>
     )
   }
