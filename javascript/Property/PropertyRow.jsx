f089624945a8a4db4ead374e8de0e18cefa7ed21
Matthew McNaney <mcnaney@gmail.com>
2017-06-29 14:47:02 -0400

    Removed component

--- javascript/Property/PropertyRow.jsx
+++ javascript/Property/PropertyRow.jsx
@@ -1,8 +1,7 @@
 'use strict'
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Row from '../Mixin/List/Row.jsx'
-
 
 export default class PropertyRow extends Row {
   constructor(props) {
