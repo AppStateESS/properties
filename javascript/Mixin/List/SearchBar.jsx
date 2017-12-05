21aad102f805ab9720a9513db91c7f630199e1ec
Matthew McNaney <mcnaney@gmail.com>
2017-12-05 14:15:25 -0500

    Removed CSSTransitionGroup
    Broken by update.

--- javascript/Mixin/List/SearchBar.jsx
+++ javascript/Mixin/List/SearchBar.jsx
@@ -3,7 +3,6 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import Dropdown from '../Form/Dropdown.jsx'
 import Amenities from '../Edit/Amenities.jsx'
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
 
 export default class SearchBar extends Component {
   constructor(props) {
@@ -21,7 +20,7 @@ export default class SearchBar extends Component {
       alpha: 'Alphabetical',
       creatednew: 'Newest',
       createdold: 'Oldest',
-      updated: 'Last update'
+      updated: 'Last update',
     }
 
     this.clearSearch = this.clearSearch.bind(this)
@@ -52,95 +51,95 @@ export default class SearchBar extends Component {
     const beds = [
       {
         label: '1+ beds',
-        handleClick: this.props.updateSearchVars.bind(null, 'beds', '1')
+        handleClick: this.props.updateSearchVars.bind(null, 'beds', '1'),
       }, {
         label: '2+ beds',
-        handleClick: this.props.updateSearchVars.bind(null, 'beds', '2')
+        handleClick: this.props.updateSearchVars.bind(null, 'beds', '2'),
       }, {
         label: '3+ beds',
-        handleClick: this.props.updateSearchVars.bind(null, 'beds', '3')
+        handleClick: this.props.updateSearchVars.bind(null, 'beds', '3'),
       }, {
         label: '4+ beds',
-        handleClick: this.props.updateSearchVars.bind(null, 'beds', '4')
+        handleClick: this.props.updateSearchVars.bind(null, 'beds', '4'),
       }, {
         label: '5+ beds',
-        handleClick: this.props.updateSearchVars.bind(null, 'beds', '5')
-      }
+        handleClick: this.props.updateSearchVars.bind(null, 'beds', '5'),
+      },
     ]
 
     const baths = [
       {
         label: '1+ baths',
-        handleClick: this.props.updateSearchVars.bind(null, 'baths', '1')
+        handleClick: this.props.updateSearchVars.bind(null, 'baths', '1'),
       }, {
         label: '2+ baths',
-        handleClick: this.props.updateSearchVars.bind(null, 'baths', '2')
+        handleClick: this.props.updateSearchVars.bind(null, 'baths', '2'),
       }, {
         label: '3+ baths',
-        handleClick: this.props.updateSearchVars.bind(null, 'baths', '3')
+        handleClick: this.props.updateSearchVars.bind(null, 'baths', '3'),
       }, {
         label: '4+ baths',
-        handleClick: this.props.updateSearchVars.bind(null, 'baths', '4')
+        handleClick: this.props.updateSearchVars.bind(null, 'baths', '4'),
       }, {
         label: '5+ baths',
-        handleClick: this.props.updateSearchVars.bind(null, 'baths', '5')
-      }
+        handleClick: this.props.updateSearchVars.bind(null, 'baths', '5'),
+      },
     ]
 
     const minprice = [
       {
         label: 'Min. Price',
-        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '0')
+        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '0'),
       }, {
         label: '$100',
-        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '100')
+        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '100'),
       }, {
         label: '$200',
-        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '200')
+        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '200'),
       }, {
         label: '$300',
-        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '300')
+        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '300'),
       }, {
         label: '$400',
-        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '400')
+        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '400'),
       }, {
         label: '$500',
-        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '500')
+        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '500'),
       }, {
         label: '$750',
-        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '750')
+        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '750'),
       }, {
         label: '$1000',
-        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '1000')
-      }
+        handleClick: this.props.updateSearchVars.bind(null, 'minprice', '1000'),
+      },
     ]
 
     const maxprice = [
       {
         label: 'Max price',
-        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '0')
+        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '0'),
       }, {
         label: '$200',
-        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '200')
+        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '200'),
       }, {
         label: '$300',
-        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '300')
+        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '300'),
       }, {
         label: '$400',
-        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '400')
+        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '400'),
       }, {
         label: '$500',
-        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '500')
+        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '500'),
       }, {
         label: '$750',
-        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '750')
+        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '750'),
       }, {
         label: '$1000',
-        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '1000')
+        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '1000'),
       }, {
         label: '$2000',
-        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '2000')
-      }
+        handleClick: this.props.updateSearchVars.bind(null, 'maxprice', '2000'),
+      },
     ]
 
     const bedLabel = `${this.props.searchVars.beds}+ beds`
@@ -168,26 +167,26 @@ export default class SearchBar extends Component {
     const sortby = [
       {
         label: this.sortTypes.rentall,
-        handleClick: this.props.updateSortBy.bind(null, 'rentall')
+        handleClick: this.props.updateSortBy.bind(null, 'rentall'),
       }, {
         label: this.sortTypes.rentunit,
-        handleClick: this.props.updateSortBy.bind(null, 'rentunit')
+        handleClick: this.props.updateSortBy.bind(null, 'rentunit'),
       }, {
         label: this.sortTypes.rentindiv,
-        handleClick: this.props.updateSortBy.bind(null, 'rentindiv')
+        handleClick: this.props.updateSortBy.bind(null, 'rentindiv'),
       }, {
         label: this.sortTypes.alpha,
-        handleClick: this.props.updateSortBy.bind(null, 'alpha')
+        handleClick: this.props.updateSortBy.bind(null, 'alpha'),
       }, {
         label: this.sortTypes.creatednew,
-        handleClick: this.props.updateSortBy.bind(null, 'creatednew')
+        handleClick: this.props.updateSortBy.bind(null, 'creatednew'),
       }, {
         label: this.sortTypes.createdold,
-        handleClick: this.props.updateSortBy.bind(null, 'createdold')
+        handleClick: this.props.updateSortBy.bind(null, 'createdold'),
       }, {
         label: this.sortTypes.updated,
-        handleClick: this.props.updateSortBy.bind(null, 'updated')
-      }
+        handleClick: this.props.updateSortBy.bind(null, 'updated'),
+      },
     ]
 
     let activeButton
@@ -206,6 +205,23 @@ export default class SearchBar extends Component {
     const searchStyle = {
       maxWidth: '300px'
     }
+
+    let amenities
+    if (this.state.fullSize === true) {
+      amenities = (
+        <div>
+          <Amenities
+            toggle={this.props.toggle}
+            searchVars={this.props.searchVars}
+            facilities={this.props.facilities}/>
+          <div className="text-center marginTop">
+            <button className="btn btn-success" onClick={this.props.clearAmenities}>Uncheck above</button>
+          </div>
+        </div>
+      )
+    }
+
+    const mt = {marginTop : '1em'}
 
     return (
       <div>
@@ -235,32 +251,16 @@ export default class SearchBar extends Component {
               <button className="btn btn-success btn-sm" onClick={this.props.resetConditions}>Reset</button>
             </div>
             {activeButton}
-            <div className="row" style={{
-              marginTop: '1em'
-            }}>
+            <div className="row" style={mt}>
               <div className="col-sm-12 text-center">
                 <button className="btn btn-default btn-sm" onClick={this.togglePanel}>{panelButton}</button>
               </div>
             </div>
-            <CSSTransitionGroup
-              transitionName="trans"
-              transitionEnterTimeout={500}
-              transitionLeaveTimeout={300}>
-              {this.state.fullSize === true
-                ? (
-                  <div>
-                    <Amenities toggle={this.props.toggle} searchVars={this.props.searchVars} facilities={this.props.facilities}/>
-                    <div className="text-center marginTop">
-                      <button className="btn btn-success" onClick={this.props.clearAmenities}>Uncheck above</button>
-                    </div>
-                  </div>
-                )
-                : null}
-            </CSSTransitionGroup>
+            {amenities}
           </div>
         </div>
         <div><Dropdown label={sortLabel} options={sortby}/></div>
-        <hr />
+        <hr/>
       </div>
     )
   }
@@ -276,5 +276,5 @@ SearchBar.propTypes = {
   updateSortBy: PropTypes.func,
   resetConditions: PropTypes.func,
   showActiveButton: PropTypes.bool,
-  facilities: PropTypes.bool
+  facilities: PropTypes.bool,
 }
