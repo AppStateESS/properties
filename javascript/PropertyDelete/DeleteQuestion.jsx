8f285cb7d51baa9bbaf6020547dd8d8cc40b20f6
Matt McNaney <mcnaneym@appstate.edu>
2019-03-25 15:57:16 -0400

    Made button blocks.

--- javascript/PropertyDelete/DeleteQuestion.jsx
+++ javascript/PropertyDelete/DeleteQuestion.jsx
@@ -14,17 +14,17 @@ export default class DeleteQuestion extends Component {
         <p>All images associated with this property will also be deleted.</p>
         <div className="mb-1">
           <button
-            className="btn btn-lg btn-danger"
+            className="btn btn-block btn-lg btn-danger"
             onClick={this.props.delete}>Yes, delete this property and all associated images.</button>
         </div>
         <div className="mb-1">
           <button
-            className="btn btn-outline-dark btn-lg btn-warning"
+            className="btn btn-block btn-outline-dark btn-lg btn-warning"
             onClick={this.props.deactivate}>Just deactivate it for now</button>
         </div>
         <div className="mb-1">
           <button
-            className="btn btn-outline-dark btn-lg"
+            className="btn btn-block btn-outline-dark btn-lg"
             onClick={this.props.close}>No, I changed my mind</button>
         </div>
       </div>
